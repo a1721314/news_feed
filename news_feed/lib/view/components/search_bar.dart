71a5c22b2a4b84b56dcde2fbd03db1d0034a2b9b
t.nagano.g <t.nagano.g@gmail.com>
2022-03-02 20:09:10 +0900

    サーチバー作成完了

--- news_feed/lib/view/components/search_bar.dart
+++ news_feed/lib/view/components/search_bar.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 
 class SearchBar extends StatelessWidget {
+  final ValueChanged onSearch;
+  final TextEditingController _textEditingController = TextEditingController();
 
-  TextEditingController _textEditingController = TextEditingController();
+  SearchBar({required this.onSearch});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 8.0,
-      shape: RoundedRectangleBorder(borderRadius: const BorderRadius.all(const Radius.circular(24.0))
-      ),
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(const Radius.circular(24.0))),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal:8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: TextField(
+          onSubmitted: onSearch,
           maxLines: 1,
           controller: _textEditingController,
           decoration: InputDecoration(
-            icon: Icon(Icons.search),
-            hintText: "検索ワードを入れてください",
-            border: InputBorder.none
-          ),
+              icon: Icon(Icons.search),
+              hintText: "検索ワードを入れてください",
+              border: InputBorder.none),
         ),
       ),
     );
--- news_feed/lib/view/screens/pages/news_list_page.dart
+++ news_feed/lib/view/screens/pages/news_list_page.dart
@@ -6,32 +6,41 @@ class NewsListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-          child: Scaffold(
-            floatingActionButton: FloatingActionButton(
-              child: Icon(Icons.refresh),
-              tooltip: "更新",
-              onPressed: () => onReflesh(context),
-                          ),
-                      body: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            //TODO 検索ワード
-                            SearchBar(),
-                            //TODO カテゴリー選択Chips
-                            //CategoryChips(),
-                            //TODO 記事表示
-                            Expanded(
-                              child: Center(
-                                child: CircularProgressIndicator(),
-                              )
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                }
-                //TODO 記事更新処理
-                onReflesh(BuildContext context) {}
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          tooltip: "更新",
+          onPressed: () => onReflesh(context),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              //TODO 検索ワード
+              SearchBar(
+                onSearch: (keyword) => getKeywordNews(context, keyword),
+              ),
+              //TODO カテゴリー選択Chips
+              //CategoryChips(),
+              //TODO 記事表示
+              Expanded(
+                  child: Center(
+                child: CircularProgressIndicator(),
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  //TODO 記事更新処理
+  onReflesh(BuildContext context) {
+    print("NewsListPage.onRefresh");
+  }
+
+  //TODO キーワード記事取得処理
+  getKeywordNews(BuildContext context, keyword) {
+    print("NewsListPage.getKeywordNews");
+  }
 }
